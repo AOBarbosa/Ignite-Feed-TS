44325d91bf29538dbbc2b7c4995037c43a917912
Andre Barbosa <andre.barbosa.076@ufrn.edu.br>
2023-02-08 19:57:40 -0300

    Att import path

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // Components
 import { Header } from "./components/Header";
-import { Sidebar } from "./components/SideBar";
+import { Sidebar } from "./components/Sidebar";
 import { Post } from "./components/Post";
 
 import styles from "./App.module.css";

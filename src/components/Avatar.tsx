e498355a4f570c35fb24605263973d333f044a2a
Andre Barbosa <andre.barbosa.076@ufrn.edu.br>
2023-02-08 19:58:57 -0300

    Adding AvatarProps

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,16 +1,23 @@
 import styles from "./Avatar.module.css";
 
+interface AvatarProps {
+  hasBorder?: boolean;
+  src: string;
+  alt?: string;
+}
+
 /**
  * Foi aplicada desestruturação nos parametros da funcao, para acessar as props
  * Dessa forma a prop hasBorder ja vai com um valor padrão true, e caso nao seja
  * passado nenhum valor para essa propriedade ela assume valor true
  */
-export function Avatar({ hasBorder = true, src }) {
+export function Avatar({ hasBorder = true, src, alt }: AvatarProps) {
   return (
     <div>
       <img
         className={hasBorder ? styles.avatarWithBorder : styles.avatar}
         src={src}
+        alt={alt}
       />
     </div>
   );
